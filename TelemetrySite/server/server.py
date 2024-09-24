b0610256793fcc443b7fc0014836cb76a366682a
trevo <tmb852@rit.edu>
2024-09-23 20:08:06 -0400

    Set up website server and client folders. Updated Requirements.txt. Started react project

---
+++ TelemetrySite/server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_cors import CORS
+
+app = Flask(__name__) #create Flask instance
+CORS(app) #Enable CORS on Flask server to work with Nodejs pages
+api = Api(app) #api router
+
+
+if __name__ == '__main__':
+    print("Starting flask")
+    app.run(debug=True), #starts Flask
