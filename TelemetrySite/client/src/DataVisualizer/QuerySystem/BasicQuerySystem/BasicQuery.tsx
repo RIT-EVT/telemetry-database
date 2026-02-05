d24b63c805af14f34267031e78a2983c8741393c
Evan <ejhingm@comcast.net>
2026-02-05 18:02:27 -0500

    updated query system basic outline

--- TelemetrySite/client/src/DataVisualizer/QuerySystem/BasicQuerySystem/BasicQuery.tsx
+++ TelemetrySite/client/src/DataVisualizer/QuerySystem/BasicQuerySystem/BasicQuery.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
     Card,
     Dropdown,
@@ -17,12 +17,16 @@ import {
 import { BuildURI } from "Utils/ServerUtils.ts";
 import QueryResponse from "./QueryResponseModal/QueryResponse";
 
+import "./BasicQuery.css";
+
 const BasicOptions = ["Date", "Date Range", "Event Name", "Event Location"];
 
 function BasicQuery() {
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
-    const [isResponseModalVisible, setResponseModalVisible] = useState(true);
+    const [response, setResponseData] = useState<object>({});
+
+    const [dataFields, setDataFields] = useState<React.ReactElement[]>([]);
 
     const [formValues, setFormValues] = useState({
         date: "",
@@ -33,23 +37,25 @@ function BasicQuery() {
     });
 
     const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
-    const toggleModal = () => setResponseModalVisible(!isResponseModalVisible);
+    const toggleModal = () => setResponseData({});
 
     const toggleOption = (option: string) => {
         setSelectedOptions((prev: string[]) => {
-            const isSelected = prev.includes(option);
+            const isCurrentlySelected = prev.includes(option);
 
-            if (option === "Date") {
-                return isSelected ? prev.filter((o) => o !== "Date") : [...prev.filter((o) => o !== "Date Range"), "Date"];
+            if (!isCurrentlySelected) {
+                if (option === "Date") {
+                    // Filter out Date Range and include Date
+                    return [...prev.filter((previousOption) => previousOption !== "Date Range"), "Date"];
+                } else if (option === "Date Range") {
+                    // Filter out Date and enable DateRange
+                    return [...prev.filter((previousOption) => previousOption !== "Date"), "Date Range"];
+                }
+
+                return [...prev, option];
             }
 
-            if (option === "Date Range") {
-                return isSelected
-                    ? prev.filter((o) => o !== "Date Range")
-                    : [...prev.filter((o) => o !== "Date"), "Date Range"];
-            }
-
-            return isSelected ? prev.filter((o) => o !== option) : [...prev, option];
+            return prev.filter((o) => o !== option);
         });
     };
 
@@ -108,8 +114,7 @@ function BasicQuery() {
         if (response.ok) {
             const data = await response.json();
 
-            console.log(data);
-            setResponseModalVisible(true);
+            setResponseData(data);
         } else {
             console.error(`An error occurred in testQuery. Fetch request returned with code ${response.status}`);
         }
@@ -142,11 +147,109 @@ function BasicQuery() {
         }
     };
 
+    useEffect(() => {
+        const getCurrentDate = () => {
+            const date = new Date();
+
+            return `${date.getFullYear()}-${(date.getMonth() < 9 ? "0" : "") + (date.getMonth() + 1)}-${(date.getDay() < 9 ? "0" : "") + (date.getDay() + 1)}`;
+        };
+        let currentDate = getCurrentDate();
+
+        // Updates the displayed inputs when the selections a new option
+        setDataFields(
+            selectedOptions.map((option) => {
+                if (option === "Date") {
+                    return (
+                        <FormGroup key={option} className='mt-3'>
+                            <Label className='basic-query-title'>Date</Label>
+                            <Input
+                                type='date'
+                                value={formValues.date}
+                                onChange={(e) => handleChange("date", e.target.value)}
+                                required
+                                max={currentDate}
+                            />
+                        </FormGroup>
+                    );
+                }
+
+                if (option === "Date Range") {
+                    return (
+                        <FormGroup key={option} className='mt-3'>
+                            <Label className='basic-query-title'>Date Range</Label>
+                            <Row>
+                                <Col md={6}>
+                                    <Input
+                                        type='date'
+                                        value={formValues.startDate}
+                                        max={formValues.endDate != "" ? formValues.endDate : currentDate}
+                                        onChange={(e) => handleChange("startDate", e.target.value)}
+                                        required
+                                    />
+                                </Col>
+                                <Col md={6}>
+                                    <Input
+                                        type='date'
+                                        value={formValues.endDate}
+                                        onChange={(e) => handleChange("endDate", e.target.value)}
+                                        min={formValues.startDate != "" ? formValues.startDate : "2000-1-1"}
+                                        max={currentDate}
+                                        required
+                                    />
+                                </Col>
+                            </Row>
+                        </FormGroup>
+                    );
+                }
+
+                if (option === "Event Name") {
+                    return (
+                        <FormGroup key={option} className='mt-3'>
+                            <Label className='basic-query-title'>Event Name</Label>
+                            <Input
+                                type='text'
+                                placeholder='Enter Name'
+                                value={formValues.eventName}
+                                onChange={(e) => handleChange("eventName", e.target.value)}
+                                required
+                            />
+                        </FormGroup>
+                    );
+                }
+
+                if (option === "Event Location") {
+                    return (
+                        <FormGroup key={option} className='mt-3'>
+                            <Label className='basic-query-title'>Event Location</Label>
+                            <Input
+                                type='text'
+                                placeholder='Enter Location'
+                                value={formValues.eventLocation}
+                                onChange={(e) => handleChange("eventLocation", e.target.value)}
+                                required
+                            />
+                        </FormGroup>
+                    );
+                }
+
+                return <FormGroup></FormGroup>;
+            }),
+        );
+    }, [
+        selectedOptions,
+        formValues.eventLocation,
+        formValues.startDate,
+        formValues.endDate,
+        formValues.date,
+        formValues.startDate,
+        formValues.eventName,
+    ]);
+
     return (
         <>
             <Form onSubmit={handleSubmit}>
                 <Card className='p-3'>
-                    {/* Dropdown */}
+                    {/* Dropdown for field selection*/}
                     <Dropdown isOpen={dropdownOpen} toggle={toggleDropdown}>
                         <DropdownToggle caret>Select Query Options</DropdownToggle>
                         <DropdownMenu>
@@ -165,80 +268,9 @@ function BasicQuery() {
                     </Dropdown>
 
                     {/* Inputs */}
-                    {selectedOptions.map((option) => {
-                        if (option === "Date") {
-                            return (
-                                <FormGroup key={option} className='mt-3'>
-                                    <Label className='basic-query-title'>Date</Label>
-                                    <Input
-                                        type='date'
-                                        value={formValues.date}
-                                        onChange={(e) => handleChange("date", e.target.value)}
-                                        required
-                                    />
-                                </FormGroup>
-                            );
-                        }
+                    {dataFields}
 
-                        if (option === "Date Range") {
-                            return (
-                                <FormGroup key={option} className='mt-3'>
-                                    <Label className='basic-query-title'>Date Range</Label>
-                                    <Row>
-                                        <Col md={6}>
-                                            <Input
-                                                type='date'
-                                                value={formValues.startDate}
-                                                onChange={(e) => handleChange("startDate", e.target.value)}
-                                                required
-                                            />
-                                        </Col>
-                                        <Col md={6}>
-                                            <Input
-                                                type='date'
-                                                value={formValues.endDate}
-                                                onChange={(e) => handleChange("endDate", e.target.value)}
-                                                required
-                                            />
-                                        </Col>
-                                    </Row>
-                                </FormGroup>
-                            );
-                        }
-
-                        if (option === "Event Name") {
-                            return (
-                                <FormGroup key={option} className='mt-3'>
-                                    <Label className='basic-query-title'>Event Name</Label>
-                                    <Input
-                                        type='text'
-                                        placeholder='Enter Name'
-                                        value={formValues.eventName}
-                                        onChange={(e) => handleChange("eventName", e.target.value)}
-                                        required
-                                    />
-                                </FormGroup>
-                            );
-                        }
-
-                        if (option === "Event Location") {
-                            return (
-                                <FormGroup key={option} className='mt-3'>
-                                    <Label className='basic-query-title'>Event Location</Label>
-                                    <Input
-                                        type='text'
-                                        placeholder='Enter Location'
-                                        value={formValues.eventLocation}
-                                        onChange={(e) => handleChange("eventLocation", e.target.value)}
-                                        required
-                                    />
-                                </FormGroup>
-                            );
-                        }
-
-                        return null;
-                    })}
-                    <Container>
+                    <Container className='top-padding'>
                         <Row xs='3'>
                             <Col className='align-center'>
                                 <Button
@@ -275,7 +307,7 @@ function BasicQuery() {
                     </Container>
                 </Card>
             </Form>
-            <QueryResponse isOpen={isResponseModalVisible} toggleModal={toggleModal} title={"Response"} />
+            <QueryResponse toggleModal={toggleModal} title={"Response"} response={response} />
         </>
     );
 }
--- TelemetrySite/client/src/DataVisualizer/QuerySystem/BasicQuerySystem/QueryResponseModal/QueryResponse.tsx
+++ TelemetrySite/client/src/DataVisualizer/QuerySystem/BasicQuerySystem/QueryResponseModal/QueryResponse.tsx
@@ -2,16 +2,17 @@ import { Modal, ModalBody, ModalHeader, ModalFooter, Button } from "reactstrap";
 import "./QueryResponse.css";
 
 type QueryResponseProps = {
-    isOpen: boolean;
     toggleModal: () => void;
     title: string;
+    response: object;
 };
 
-const QueryResponse = ({ isOpen, toggleModal, title }: QueryResponseProps) => {
+const QueryResponse = ({ toggleModal, title, response }: QueryResponseProps) => {
+    console.log(response);
     return (
-        <Modal isOpen={isOpen} toggle={toggleModal}>
+        <Modal isOpen={Object.keys(response).length !== 0} toggle={toggleModal}>
             <ModalHeader className='background'>{title}</ModalHeader>
-            <ModalBody className='background'>{/* response content */}</ModalBody>
+            <ModalBody className='background'>{}</ModalBody>
             <ModalFooter className='background'>
                 <Button onClick={toggleModal}></Button>
             </ModalFooter>
