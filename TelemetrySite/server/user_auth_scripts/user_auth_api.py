2041352dc87a31473840dd6ee667bb5af2f7d587
Evan <ejhingm@comcast.net>
2026-03-31 18:07:37 -0400

    fixed tests

--- TelemetrySite/server/user_auth_scripts/user_auth_api.py
+++ TelemetrySite/server/user_auth_scripts/user_auth_api.py
@@ -7,41 +7,44 @@ from datetime import datetime
 
 from http_codes import HttpResponseType
 
-class UserAuthApi(MethodView):   
+
+class UserAuthApi(MethodView):
     def __init__(self, db):
         self.db = db
 
     def post(self):
         user_data = request.get_json()
-        
+
         user_db_connection = self.db["users"]
         match user_data.get("action"):
             case "login":
-                
+
                 # Check the username and password against the db to make sure the user
-                # exists and they are who they say they are. Return auth token if the 
+                # exists and they are who they say they are. Return auth token if the
                 # values are correct, otherwise return an invalid user error
-                
+
                 username = user_data.get("name")
                 password = user_data.get("password")
-                
-                mongo_data = user_db_connection.find_one({"username":username, "password":password.encode()})
-            
+
+                mongo_data = user_db_connection.find_one(
+                    {"username": username, "password": password.encode()}
+                )
+
                 if mongo_data == None:
-                    return HttpResponseType.UNAUTHORIZED.error()      
+                    return HttpResponseType.UNAUTHORIZED.error()
                 else:
                     auth_token = mongo_data["auth_token"]
                     # always update auth token on login
                     auth_token = update_expired_token(mongo_data["_id"], self.db)
-                    return {"auth_token": auth_token}, HttpResponseType.OK.value   
+                    return {"auth_token": auth_token}, HttpResponseType.OK.value
             case "signup":
-                
+
                 username = user_data.get("name")
-                password = user_data.get("password")  
+                password = user_data.get("password")
                 secure_num = user_data.get("secureNum")
-                
-                current_date_time = datetime.now()   
-                
+
+                current_date_time = datetime.now()
+
                 if secure_num != 1:
                     # User value doesn't match the needed value
                     # return an error message
@@ -50,13 +53,19 @@ class UserAuthApi(MethodView):
                 elif username == "" or password == "":
                     return HttpResponseType.UNAUTHORIZED.error()
                 # Ensure there isn't any duplicate usernames
-                elif user_db_connection.find_one({"username":username})!=None:
+                elif user_db_connection.find_one({"username": username}) != None:
                     return HttpResponseType.UNAUTHORIZED.error()
                 else:
-                    auth_token = create_auth_token(self.db)    
-                    user_db_connection.insert_one({"username" : username, "password" : password.encode(),
-                                                    "auth_token" : auth_token, "auth_time" : current_date_time})
-                    
-                    return {"auth_token":auth_token}, HttpResponseType.CREATED.value    
+                    auth_token = create_auth_token(self.db)
+                    user_db_connection.insert_one(
+                        {
+                            "username": username,
+                            "password": password.encode(),
+                            "auth_token": auth_token,
+                            "auth_time": current_date_time,
+                        }
+                    )
+
+                    return {"auth_token": auth_token}, HttpResponseType.CREATED.value
             case _:
                 return HttpResponseType.NOT_FOUND.error()
